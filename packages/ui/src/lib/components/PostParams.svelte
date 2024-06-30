cb1a88968139ed9a6cec959943af8ed111859962
Shane Carroll <carrollshaned@gmail.com>
2024-06-30 12:58:45 -0400

    fix post param rendering on mobile and clicking off to close

--- packages/ui/src/lib/components/PostParams.svelte
+++ packages/ui/src/lib/components/PostParams.svelte
@@ -6,7 +6,6 @@
 	export let onParamsChange: (p: ContentParams) => void;
 
 	function onParamChange(changedParam: ContentParam<ParamType>) {
-		console.log('params change fired!', changedParam);
 		if (params && onParamsChange) {
 			const newParams = params.map((p) => (p.id === changedParam.id ? changedParam : p));
 			onParamsChange(newParams);
@@ -16,7 +15,7 @@
 </script>
 
 <div
-	class="fixed right-12 max-sm:right-6 top-[20vh] min-h-[40vh] max-h-[75vh] w-[17vw] bg-surface rounded-md shadow-lg p-4 overflow-y-scroll"
+	class="fixed right-12 max-sm:right-6 top-[20vh] min-h-[40vh] max-h-[75vh] md:w-[17vw] max-sm:w-[50vw] bg-surface rounded-md shadow-lg p-4 overflow-y-scroll"
 >
 	{#each params as param}
 		<div class="flex my-2.5">
